8ab9a4b493dd7f4cb65b10b8f3c91ba1a1f4e26c
Giacomo Tagliabue <giacomo.tag@gmail.com>
2016-08-01 18:24:17 -0400

    v0.1.2

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@ from setuptools import setup
 
 setup(
     name='flask_resty_swagger',
-    version='0.1.1',
+    version='0.1.2',
     description="Generate swagger documentation from flask-resty service",
     author="Giacomo Tagliabue",
     author_email="giacomo.tag@gmail.com",
     license="MIT",
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ),
     keywords="rest flask swagger",
     packages=['flask_resty_swagger'],
     zip_safe=False,
